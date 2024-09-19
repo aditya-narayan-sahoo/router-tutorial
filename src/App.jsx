cce516e216c1155817a538f863ed887e89d874d9
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-19 11:17:01 +0530

    Add Routing Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
   return (
-    <div>
-      <h1 className="underline text-5xl font-serif">ANS</h1>
-    </div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import User from "./components/User";
+import Github from "./components/Github";
 
-createRoot(document.getElementById('root')).render(
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userId" element={<User />} />
+      <Route path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </StrictMode>
+);
+
